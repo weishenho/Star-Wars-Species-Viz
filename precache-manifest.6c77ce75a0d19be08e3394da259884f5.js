affe1773294d428d5a0dac172953aaf696ddd4bf
weishenho <jijiwawaa@gmail.com>
2020-07-14 00:05:05 +0800

    build

---
+++ precache-manifest.6c77ce75a0d19be08e3394da259884f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f54685be96088e9c62ed093f5c67099",
+    "url": "/Star-Wars-Species-Viz/index.html"
+  },
+  {
+    "revision": "3d00e9ff1761b40cd22e",
+    "url": "/Star-Wars-Species-Viz/static/css/main.2b312470.chunk.css"
+  },
+  {
+    "revision": "5b398a42010128a5be83",
+    "url": "/Star-Wars-Species-Viz/static/js/2.f2c00610.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/Star-Wars-Species-Viz/static/js/2.f2c00610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d00e9ff1761b40cd22e",
+    "url": "/Star-Wars-Species-Viz/static/js/main.e040bf26.chunk.js"
+  },
+  {
+    "revision": "b7ffb7809fef4afd70fb",
+    "url": "/Star-Wars-Species-Viz/static/js/runtime-main.b4afad0d.js"
+  }
+]);
